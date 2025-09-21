69960ad61c2befe5ac9b2b8a5aaad184aa66b607
Kaua Barroso <115954731+Ka1ux@users.noreply.github.com>
2025-09-21 03:40:59 -0300

    Add files via upload

--- Numero_maior/Numero_maior.c
+++ Numero_maior/Numero_maior.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int numeros[5];
+    int i, maior;
+
+    for (i = 0; i < 5; i++) {
+        printf("Digite um numero: ");
+        scanf("%d", &numeros[i]);
+    }
+
+    maior = numeros[0];
+
+    for (i = 1; i < 5; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+    }
+
+    printf("\nO maior numero digitado foi: %d\n", maior);
+
+    return 0;
+}
